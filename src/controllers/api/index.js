c5358c59e1b328c4e5fb68b0693fd54f4b725e49
roxywasiak <ruksclone@hotmail.com>
2022-06-19 21:55:37 +0100

    added the skeleton functions for the cretae notes feature

--- src/controllers/api/index.js
+++ src/controllers/api/index.js
@@ -1,11 +1,23 @@
 //import your notes and utils
+const { v4: uuidv4 } = require("uuid");
+const { readFromFile, writeToFile } = require("../../utils");
 
-//get request
+//get request for api route
+const getNotes = () => {};
 
 //post request
+const postNotes = () => {};
 
 //put request
+const updateNotes = () => {};
 
 //delete request
+const deleteNotes = () => {};
 
 //export the items
+module.exports = {
+  getNotes,
+  postNotes,
+  updateNotes,
+  deleteNotes,
+};
