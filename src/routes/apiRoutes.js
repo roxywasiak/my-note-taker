ee893088b8aef4abf71d04a831e09d85275e59a7
roxywasiak <ruksclone@hotmail.com>
2022-06-19 21:49:44 +0100

    set up the controllers and get all notes api

--- src/routes/apiRoutes.js
+++ src/routes/apiRoutes.js
@@ -1,9 +1,24 @@
 //get alll the crud operations
+const { Router } = require("express");
+const { get } = require("./viewRoutes");
 
 //list all the routers
+const {
+  getNotes,
+  postNotes,
+  updateNotes,
+  deleteNotes,
+} = require("../controllers/api");
 //getNotes
 //postNotes
 //updateNotes
 //deleteNotes
 
+const newRouter = Router();
+newRouter.get();
+newRouter.post();
+newRouter.put();
+newRouter.delete();
+
 //export the modules
+module.exports = newRouter;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,3 +1,13 @@
 //get the two routes in
+const { Router } = require("express");
+
+const apiRoutes = require("./apiRoutes");
+const viewRoutes = require("./viewRoutes");
+
+const newRouter = Router();
+
+newRouter.use("/api", apiRoutes);
+newRouter.use("/", viewRoutes);
 
 //export modules
+module.exports = newRouter;
